09e41f4552f43483821f824719afd2379902a5a4
stefanNaumov <stefannaumow@gmail.com>
2014-07-11 13:51:09 +0300

    change IMoveChecker to IMoveHandler.cs

---
+++ Labyrinth/IMoveHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth
+{
+    public interface IMoveHandler
+    {
+        bool TryMove(Command direction, ILabyrinth labyrinth);
+    }
+}
