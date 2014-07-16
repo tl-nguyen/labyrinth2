24de53c0077143f4c12bbe5c02cef37286507e48
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-07-16 10:04:54 +0300

    added unit tests for FileSerializationManager class

---
+++ Labyrinth.Tests/FileSerializationManagerTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Labyrinth.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Runtime.Serialization.Formatters.Binary;
+    using System.Runtime.Serialization.Formatters.Soap;
+
+    [TestClass]
+    public class FileSerializationManagerTests
+    {
+        [TestMethod]
+        public void TestFileSerializationManagerConstructorCreateInstance()
+        {
+            var manager = new FileSerializationManager(new BinaryFormatter(), "test.bin");
+            Assert.IsInstanceOfType(manager, typeof(FileSerializationManager));
+        }
+
+        [TestMethod]
+        public void TestFileSerializationManagerGetFileName()
+        {
+            var manager = new FileSerializationManager(new BinaryFormatter(), "test.bin");
+            Assert.AreEqual(manager.FileName, "test.bin");
+        }
+
+        [TestMethod]
+        public void TestFileSerializationManagerGetFormatter()
+        {
+            var soapFormatter = new SoapFormatter();
+            var manager = new FileSerializationManager(soapFormatter, "test.bin");  
+            Assert.AreEqual(manager.Formatter, soapFormatter);
+        }
+
+        [TestMethod]
+        public void TestFileSerializationManagerSerializeDeserialize()
+        {
+            var manager = new FileSerializationManager(new BinaryFormatter(), "test.bin");
+            var table = new TopResults();
+            table.Add(new Result(2, "player"));
+            table.Add(new Result(1, "player"));
+            manager.Serialize(table);
+            var afterSerializationTable = manager.Deserialize();
+            Assert.AreEqual(table.ToString(), afterSerializationTable.ToString());
+        }
+    }
+}
