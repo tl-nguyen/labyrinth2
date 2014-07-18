22f7dc98c214afb5dbcb718c686f9f8f0d57ce69
kizisoft <kizisoft@gmail.com>
2014-07-18 16:18:09 +0300

    Labyrinth.Tests - initial

---
+++ Labyrinth.Tests/LoggersTests/LoggersAndAppendersTest.cs
@@ -0,0 +1,38 @@
+﻿namespace Labyrinth.Tests.LoggersTests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Loggers;
+
+    /// <summary>
+    /// Summary description for FileAppenderTest
+    /// </summary>
+    [TestClass]
+    public class LoggersAndAppendersTest
+    {
+        private const string FileName = "TestLog.txt";
+        private const string TestMessage = "Loggers Test";
+
+        private IAppender fileAppender;
+        private IAppender memoryAppender;
+        private ILogger simpleLoggerFileAppender;
+        private ILogger simpleLoggerMemoryAppender;
+
+        [TestInitialize()]
+        public void TestInitializeInstances()
+        {
+            this.fileAppender = new FileAppender(FileName);
+            this.memoryAppender = MemoryAppender.GetInstance();
+            this.simpleLoggerFileAppender = new SimpleLogger(this.fileAppender);
+            this.simpleLoggerMemoryAppender = new SimpleLogger(this.memoryAppender);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
