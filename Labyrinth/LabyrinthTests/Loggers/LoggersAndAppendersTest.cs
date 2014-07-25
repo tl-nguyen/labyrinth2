63f95b290650c2690ebab6bfd08cb1f8893b99c2
kizisoft <kizisoft@gmail.com>
2014-07-25 12:06:30 +0300

    This file was changed in the original repository with an older version - PLEASE LOOK AT THE CHANGES OF THE OTHERS BEFORE MAJOR MODIFICATIONS!

--- Labyrinth/LabyrinthTests/Loggers/LoggersAndAppendersTest.cs
+++ Labyrinth/LabyrinthTests/Loggers/LoggersAndAppendersTest.cs
@@ -1,17 +1,20 @@
-﻿namespace Labyrinth.Tests.Loggers
+﻿namespace Labyrinth.Tests.LoggersTests
 {
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Labyrinth.Loggers.Contracts;
-    using Labyrinth.Loggers;
+    using Loggers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.IO;
 
     /// <summary>
-    /// Summary description for FileAppenderTest
+    /// Summary Here are the tests for loggers and appenders
     /// </summary>
     [TestClass]
     public class LoggersAndAppendersTest
     {
         private const string FileName = "TestLog.txt";
         private const string TestMessage = "Loggers Test";
+        private const ulong DefaultCount = 0;
 
         private IAppender fileAppender;
         private IAppender memoryAppender;
@@ -21,16 +24,42 @@
         [TestInitialize()]
         public void TestInitializeInstances()
         {
-            this.fileAppender = new FileAppender(FileName);
-            this.memoryAppender = MemoryAppender.GetInstance();
-            this.simpleLoggerFileAppender = new SimpleLogger(this.fileAppender);
-            this.simpleLoggerMemoryAppender = new SimpleLogger(this.memoryAppender);
+            this.fileAppender = LabyrinthFactory.GetFileAppender(FileName);
+            this.memoryAppender = LabyrinthFactory.GetMemoryAppender();
+            this.simpleLoggerFileAppender = LabyrinthFactory.GetSimpleLogger(this.fileAppender);
+            this.simpleLoggerMemoryAppender = LabyrinthFactory.GetSimpleLogger(this.memoryAppender);
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestFileAppender()
         {
+            this.simpleLoggerFileAppender.Log(TestMessage);
+            Assert.AreEqual(DefaultCount + 1, this.fileAppender.MessageCount, "Logged messages are not count correctly!");
+        }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestFileAppenderNullCreation()
+        {
+            var fileAppender = LabyrinthFactory.GetFileAppender(string.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestSimpleLoggerWithFileAppenderNullCreation()
+        {
+            var simpleLogger = LabyrinthFactory.GetSimpleLogger(null);
+        }
+
+        [TestMethod]
+        public void TestSimpleLoggerWithFileAppenderLog()
+        {
+            File.Delete(FileName);
+            this.simpleLoggerFileAppender.Log(TestMessage);
+            var stream = new StreamReader(FileName);
+            var readedMessage = stream.ReadLine();
+            var result = readedMessage.Contains(TestMessage);
+            Assert.AreEqual(true, result, "The SimpleLoggerWithFileAppender does not write correctly in the file!");
         }
     }
 }
