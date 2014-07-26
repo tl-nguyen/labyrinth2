d024218a924afc94efc78be7b9425e666eb9267c
iliantrifonov <shogun11@abv.bg>
2014-07-26 17:51:02 +0300

    StyleCop and documentation for GameEngine.cs

--- Labyrinth/Labyrinth/GameEngine.cs
+++ Labyrinth/Labyrinth/GameEngine.cs
@@ -1,12 +1,12 @@
 ﻿namespace Labyrinth
 {
+    using System;
+    using Commons;
     using Entities;
     using Entities.Contracts;
     using Loggers.Contracts;
     using Renderer.Contracts;
-    using System;
     using UI.Contracts;
-    using Commons;
 
     /// <summary>
     /// Class that gives the game objects to different modules, and transfers commands from one class to another, allowing them to be detached.
@@ -33,6 +33,12 @@
         private IRenderable labyrinthGraphic;
         private IRenderable tableGraphic;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameEngine"/> class
+        /// </summary>
+        /// <param name="width">Integer value for the width</param>
+        /// <param name="height">Integer value for the height</param>
+        /// <param name="factory">A non null instance of <see cref="IFactory"/></param>
         public GameEngine(int width, int height, IFactory factory)
         {
             this.windowWidth = width;
@@ -40,7 +46,9 @@
             this.factory = factory;
             this.input = this.factory.GetUserInputInstance();
             this.labyrinth = this.factory.GetLabyrinthInstance(factory, this.factory.GetMoveHandlerInstance());
-            this.gameConsole = new GameConsole(this.factory.GetLanguageStringsInstance());//TODO: use factory
+
+            // TODO: use factory
+            this.gameConsole = new GameConsole(this.factory.GetLanguageStringsInstance());
             this.resultsTable = this.factory.GetTopResultsTableInstance();
 
             this.resultsTable.Table.Changed += (sender, e) =>
@@ -55,19 +63,18 @@
             this.renderer = this.consoleGraphicFactory.GetConsoleRenderer();
             this.scene = this.factory.GetConsoleScene(this.renderer);
 
-            this.labyrinthGraphic = this.consoleGraphicFactory.GetLabyrinthConsoleGraphic(this.labyrinth,
-                this.consoleGraphicFactory.GetCoordinates(2, 1), this.renderer);
-            this.gameConsoleGraphic = this.consoleGraphicFactory.GetGameConsoleGraphic(this.gameConsole,
-                this.consoleGraphicFactory.GetCoordinates(2, this.labyrinth.LabyrinthSize + 2), this.renderer);
-            this.tableGraphic = this.consoleGraphicFactory.GetResultsTableConsoleGraphic(this.resultsTable,
-                this.consoleGraphicFactory.GetCoordinates(2, 1), this.renderer);
+            this.labyrinthGraphic = this.consoleGraphicFactory.GetLabyrinthConsoleGraphic(this.labyrinth, this.consoleGraphicFactory.GetCoordinates(2, 1), this.renderer);
+            this.gameConsoleGraphic = this.consoleGraphicFactory.GetGameConsoleGraphic(this.gameConsole, this.consoleGraphicFactory.GetCoordinates(2, this.labyrinth.LabyrinthSize + 2), this.renderer);
+            this.tableGraphic = this.consoleGraphicFactory.GetResultsTableConsoleGraphic(this.resultsTable, this.consoleGraphicFactory.GetCoordinates(2, 1), this.renderer);
 
             this.gameLogic = factory.GetGameLogic(this.labyrinth, this.gameConsole, this.resultsTable, this.input, this.factory);
         }
 
         /// <summary>
-        /// Width and height units depend on the specific renderer. In the case of a Console renderer they are in console rows and cols.
+        /// Initializes a new instance of the <see cref="GameEngine"/> class
         /// </summary>
+        /// <param name="width">Integer value for the width</param>
+        /// <param name="height">Integer value for the height</param>
         public GameEngine(int width, int height)
             : this(width, height, new Factory())
         {
@@ -103,7 +110,7 @@
             this.scene.Add(this.gameConsoleGraphic);
             this.gameConsole.AddInput(Dialog.Welcome);
             this.gameConsole.AddInput(Dialog.Input);
-            scene.Render();
+            this.scene.Render();
         }
 
         private void ExitApplication()
