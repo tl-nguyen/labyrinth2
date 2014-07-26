014bd85c9ad2d1135c5a4338fba182e5af061e56
kalinalazarova1 <kalina.lazarova@gmail.com>
2014-07-27 00:10:08 +0300

    added tests for GameConsole class

--- Labyrinth/LabyrinthTests/Entities/GameConsoleTests.cs
+++ Labyrinth/LabyrinthTests/Entities/GameConsoleTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labyrinth.Entities;
+using Labyrinth.Renderer;
 
 namespace Labyrinth.Tests.Entities
 {
@@ -7,8 +10,23 @@ namespace Labyrinth.Tests.Entities
     public class GameConsoleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGameConsoleAddInput()
         {
+            var console = new GameConsole(new LanguageStrings());
+            console.AddInput(Commons.Dialog.GoodBye);
+            var actual = console.GetLastLines(1);
+            string[] expected = {"Good Bye\nPress any key to exit...\n "};
+            Assert.IsTrue(actual.SequenceEqual<string>(expected));
+        }
+
+        [TestMethod]
+        public void TestGameConsoleAddInputWithArgs()
+        {
+            var console = new GameConsole(new LanguageStrings());
+            console.AddInput(Commons.Dialog.WinMessage, new string[]{"5"});
+            var actual = console.GetLastLines(1);
+            string[] expected = { "Congratulations! You escaped in 5 moves. " };
+            Assert.IsTrue(actual.SequenceEqual<string>(expected));
         }
     }
 }
